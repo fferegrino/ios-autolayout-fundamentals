380b9353b31e9a04e7c2c6e58f73b0a2cd1a5ef8
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-07-08 06:51:28 -0500

    VennDiagram project.

---
+++ VennDiagram/VennDiagram/CircleView.cs
@@ -0,0 +1,73 @@
+﻿//
+//  CircleView.swift
+//  VennDiagram
+//
+//  Created by James Wilson on 12/16/15.
+//  Copyright © 2015 Pluralsight. All rights reserved.
+//
+
+using System;
+using System.ComponentModel;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace VennDiagram
+{
+	[Register("CircleView"), DesignTimeVisible(true)]
+	public class CircleView : UIView
+	{
+		private UIColor _fillColor;
+
+		[Export("FillColor"), Browsable(true)]
+		public UIColor FillColor
+		{
+			get { return _fillColor; }
+			set
+			{
+				_fillColor = value;
+				LayoutSubviews();
+				LayoutIfNeeded();
+			}
+		}
+
+		public CircleView(IntPtr p) : base(p)
+		{
+			Initialize();
+		}
+
+		public void Initialize()
+		{
+			FillColor = UIColor.LightTextColor;
+			BackgroundColor = UIColor.Clear;
+		}
+
+		public CircleView(NSCoder aDecoder) : base(aDecoder)
+		{
+			Initialize();
+		}
+
+		public CircleView(CGRect frame) : base(frame)
+		{
+			Initialize();
+		}
+
+		public CAShapeLayer ShapeLayer => Layer as CAShapeLayer;
+
+		[Export("layerClass")]
+		public static Class LayerClass()
+		{
+			return new Class(typeof(CAShapeLayer));
+		}
+
+		public override void LayoutSubviews()
+		{
+			base.LayoutSubviews();
+			var beizerPath = UIBezierPath.FromOval(Bounds);
+			ShapeLayer.FillColor = FillColor.CGColor;
+			ShapeLayer.Path = beizerPath.CGPath;
+		}
+	}
+}
---
+++ VennDiagram/VennDiagram/VennDiagram.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UIKit;
+using System.ComponentModel;
+
+namespace VennDiagram
+{
+	public enum VennWeight
+	{
+		Left,
+		Balanced,
+		Right
+	}
+
+	[Foundation.Register("VennDiagram"), DesignTimeVisible(true)]
+	public class VennDiagram : UIView
+	{
+		public NSLayoutConstraint LeftCircleTopConstraint { get; set; }
+		public NSLayoutConstraint LeftCircleLeadingConstraint { get; set; }
+		public NSLayoutConstraint LeftCircleBottomConstraint { get; set; }
+
+		public NSLayoutConstraint RightCircleTopConstraint { get; set; }
+		public NSLayoutConstraint RightCircleTrailingConstraint { get; set; }
+		public NSLayoutConstraint RightCircleBottomConstraint { get; set; }
+
+
+
+		public VennDiagram(IntPtr p) : base(p)
+        {
+		}
+
+		private VennWeight _Weight = VennWeight.Balanced;
+		public VennWeight Weight
+		{
+			get { return _Weight; }
+			set { _Weight = value; SetNeedsUpdateConstraints(); }
+		}
+
+		public override void AwakeFromNib()
+		{
+			var view = new UIView();
+			view.Frame = Bounds;
+			view.BackgroundColor = UIColor.FromRGBA((nfloat)0.9, (nfloat)0.9, (nfloat)0.9, (nfloat)1.0);
+			view.Layer.CornerRadius = (nfloat)12.0;
+			view.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+
+			view.TranslatesAutoresizingMaskIntoConstraints = false;
+
+			InsertSubview(view, 0);
+		}
+
+		public override void UpdateConstraints()
+		{
+			if (LeftCircleBottomConstraint == null)
+			{
+				base.UpdateConstraints();
+				return;
+			}
+			switch (Weight)
+			{
+				case VennWeight.Left:
+					this.LeftCircleBottomConstraint.Constant = 20;
+					this.LeftCircleLeadingConstraint.Constant = 20;
+					this.LeftCircleTopConstraint.Constant = 20;
+
+					this.RightCircleBottomConstraint.Constant = 40;
+					this.RightCircleTrailingConstraint.Constant = 40;
+					this.RightCircleTopConstraint.Constant = 40;
+					break;
+				case VennWeight.Balanced:
+					this.LeftCircleBottomConstraint.Constant = 20;
+					this.LeftCircleLeadingConstraint.Constant = 20;
+					this.LeftCircleTopConstraint.Constant = 20;
+
+					this.RightCircleBottomConstraint.Constant = 20;
+					this.RightCircleTrailingConstraint.Constant = 20;
+					this.RightCircleTopConstraint.Constant = 20;
+					break;
+				case VennWeight.Right:
+					this.LeftCircleBottomConstraint.Constant = 40;
+					this.LeftCircleLeadingConstraint.Constant = 40;
+					this.LeftCircleTopConstraint.Constant = 40;
+
+					this.RightCircleBottomConstraint.Constant = 20;
+					this.RightCircleTrailingConstraint.Constant = 20;
+					this.RightCircleTopConstraint.Constant = 20;
+					break;
+			}
+			base.UpdateConstraints();
+		}
+	}
+}
